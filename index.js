c6407ce41d7be6f0f8af03a2030d0f3fbc4f6e73
ShanteTorres <kennedy2110@gmail.com>
2021-12-17 17:11:27 -0500

    commit tree 0841850adefd58ce1f1333a21e6597494d9fde07

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+ superbowlWin = (record) => {
+  const result = record.find((record) => record.result === "W"); 
+  return !!result ? result.year : undefined;
+  //if(result){
+ //  return result.year
+ // } else {
+  //  return undefined 
+ // }
+}
+
+//record.find(superbowlWin);
+
+//Array.find(record).year
